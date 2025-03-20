179f64309e4c50e9e19e59a110537bd1eb4c3a11
jchavarrias <chavarriasan.javier@gmail.com>
2025-03-20 18:44:27 +0100

    arreglo el código, para que se importen las cosas

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,6 @@
+import polinomios
+import ficheros
+
 def menu_opciones():
     """Muestra el menú de opciones y ejecuta la operación elegida."""
     print("\033[1m¡Bienvenido a la calculadora de polinomios!\033[0m")
