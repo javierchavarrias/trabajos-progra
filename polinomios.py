3b521ab7d8a51d3e63129872933655fce099a824
jchavarrias <chavarriasan.javier@gmail.com>
2025-03-20 17:35:27 +0100

    creo  la estructura de los poliniomios y la inicialización

--- polinomios.py
+++ polinomios.py
@@ -5,7 +5,4 @@ class Polinomio: #creamos un diccionario nueva para almacenar los polinomios
     def __str__(self):
         """Devuelve la representación en cadena del polinomio."""
         return " + ".join([f"{coef}x^{grado}" if grado > 1 else (f"{coef}x" if grado == 1 else f"{coef}") for grado, coef in sorted(self.coeficientes.items(), reverse=True)])
-git add .
-git commit -m "Añadiendo archivos nuevos"
-git push origin main
 
