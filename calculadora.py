b5ea0e26aec15d2ecbdceec99ed14a5319c1b033
jchavarrias <chavarriasan.javier@gmail.com>
2025-03-20 18:21:15 +0100

    creo el menu que va  a ser la interfaz y hago el print inicial

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+def menu_opciones():
+    """Muestra el menú de opciones y ejecuta la operación elegida."""
+    print("\033[1m¡Bienvenido a la calculadora de polinomios!\033[0m")
+    print("-------------------")
