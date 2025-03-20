18436b24111a532a317754fbebfcd9bfd17a83ec
dglprieto <dglprieto@gmail.com>
2025-03-20 17:37:21 +0100

    creo la evaluación del polinomio

--- polinomios.py
+++ polinomios.py
@@ -6,3 +6,7 @@ class Polinomio: #creamos un diccionario nueva para almacenar los polinomios
         """Devuelve la representación en cadena del polinomio."""
         return " + ".join([f"{coef}x^{grado}" if grado > 1 else (f"{coef}x" if grado == 1 else f"{coef}") for grado, coef in sorted(self.coeficientes.items(), reverse=True)])
 
+    def evaluar(self, x):
+        """Evalúa el polinomio en un valor x dado."""
+        resultado = sum(coef * (x ** grado) for grado, coef in self.coeficientes.items())
+        return resultado
